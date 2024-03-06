1e36277335e96b472df6dce4c63410454f1ced84
EfeFatih52 <146707128+EfeFatih52@users.noreply.github.com>
2024-03-06 12:16:33 +0300

    Add files via upload

---
+++ GirisKontrol.php
@@ -0,0 +1,42 @@
+<?php
+ $yonetim = array(
+ array( "ad" => "Melih", "sifre" => "123"),
+ array( "ad" => "Hüma", "sifre" => "456"),
+ array( "ad" => "Afra", "sifre" => "789"),
+ array( "ad" => "Göktuğ", "sifre" => "741"),
+ );
+ $kullanici=$_POST['kAdi'];
+ $sifre=$_POST['sifre'];
+ if(empty($kullanici)||empty($sifre))
+ {
+ echo "<div class='bosGiris'><h2>Bilgileriniz eksiktir. <br> Giriş
+sayfasına yönlendiriliyorsunuz.</h2></div>";
+ header("Refresh: 5;url=login.php"); 
+ }
+ else{ 
+ for ($i=0; $i <count($yonetim); $i++)
+ {
+ if($yonetim[$i]["ad"]==$kullanici && $yonetim[$i]["sifre"]==$sifre )
+ {
+ $indis=$i;
+ break;
+ }
+ else
+ {
+ $indis=-1;
+ }
+ }
+ if($indis==-1)
+ {
+ echo "<div><h2>Sistemde Kaydınız bulunmamaktadır.<br>Giriş
+sayfasına yönlendiriliyorsunuz.</h2> </div>";
+ header("Refresh: 5; url=login.php"); 
+ }
+ if($indis>-1)
+ {
+ echo "<div class=‘dogruGiris’><h2>Merhaba".
+$yonetim[$indis]["ad"].",<br>Sisteme Girişiniz Başarıyla Gerçekleşmiştir.
+</h2></div>";
+ }
+ } 
+?>
